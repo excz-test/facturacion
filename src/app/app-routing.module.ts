a1ae9156b240b8c1ca7d85f6ac5cb7b3dc16d103
Edu <excz.test@gmail.com>
2018-09-27 23:30:49 -0500

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AddCardComponent } from './pages/add-card/add-card.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'details/:city', component: DetailsComponent},
+  {path: 'add', component: AddCardComponent},
+  /*{path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},*/
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
